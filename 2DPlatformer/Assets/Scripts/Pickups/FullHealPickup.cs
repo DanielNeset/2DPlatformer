6901be59dfd58ea6ca5bb94b6630254696ad9dda
Daniel Neset <danielneset@gmail.com>
2024-09-22 22:57:55 +0200

    Finished the assignment

--- 2DPlatformer/Assets/Scripts/Pickups/FullHealPickup.cs
+++ 2DPlatformer/Assets/Scripts/Pickups/FullHealPickup.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class FullHealPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+            playerHealth.ReceiveHealing(playerHealth.maximumHealth);
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
